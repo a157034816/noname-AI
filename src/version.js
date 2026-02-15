9229115b15bb6e6e9d4e9e9c5d1650b5a9e80212
放屁无声还很臭 <a157034816@outlook.com>
2026-02-15 11:42:06 +0800

    更新0.0.2

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const SLQJ_AI_EXTENSION_NAME = "身临其境的AI";
-export const SLQJ_AI_EXTENSION_VERSION = "0.0.1";
+export const SLQJ_AI_EXTENSION_VERSION = "0.0.2";
 
 export const SLQJ_AI_UPDATE_REPO = {
   owner: "a157034816",
@@ -7,4 +7,3 @@ export const SLQJ_AI_UPDATE_REPO = {
 };
 
 export const SLQJ_AI_UPDATE_ZIP_ROOT_NAME = "身临其境的AI";
-
